ee7bb1a13510c2df01775500edc33ee11e0549d4
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-06-08 20:55:00 +0700

    chore: add comment for each config object

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js',
   },
+  // location that used by webpack to put result of pack
   devServer: {
     contentBase: './dist',
   },
+  // loader rules for babel and eslint
   module: {
     rules: [{
       test: /\.(js|jsx)$/,
@@ -23,7 +25,8 @@ module.exports = {
     },
     ],
   },
+  // To enable import without extension on js file
   resolve: {
-    extensions: ['.js', 'j.sx'],
+    extensions: ['.js', '.jsx'],
   },
 };
